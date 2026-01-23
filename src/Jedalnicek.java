cb911fc5ce4c33f96b8890a3861bddbc7e0e204c
Filip Lohyňa <filip.lohyna@gmail.com>
2026-01-23 13:34:33 +0100

    Jedalnicek a katalog

--- src/Jedalnicek.java
+++ src/Jedalnicek.java
@@ -7,4 +7,28 @@ public class Jedalnicek {
     public Jedalnicek() {
         this.jedla = new ArrayList<>();
     }
+
+    public void pridajJedlo(Jedlo jedlo) {
+        jedla.add(jedlo);
+    }
+
+    public List<Jedlo> getJedla() {
+        return jedla;
+    }
+
+    public double getSpoluKalorie() {
+        return jedla.stream().mapToDouble(Jedlo::getKcal).sum();
+    }
+
+    public double getSpoluBielkoviny() {
+        return jedla.stream().mapToDouble(Jedlo::getBielkoviny).sum();
+    }
+
+    public double getSpoluTuky() {
+        return jedla.stream().mapToDouble(Jedlo::getTuky).sum();
+    }
+
+    public double getSpoluSacharidy() {
+        return jedla.stream().mapToDouble(Jedlo::getUhlovodraty).sum();
+    }
 }
